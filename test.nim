536b2c9f683e7349b0cc234ed91761780aca3713
drkameleon <yaniszaf@gmail.com>
2022-02-14 14:06:26 +0100

    update

--- test.nim
+++ test.nim
@@ -3,6 +3,8 @@ import webview
 var wt: WebviewT
 
 wt = createWebview()
+wt.setWebviewTitle("TEST")
+wt.setWebviewSize(400,300,0)
 wt.navigateWebview("data:text/html, <h1>It worked! Yay!</h1>")
 wt.runWebview()
 wt.destroyWebview()
--- webview.nim
+++ webview.nim
@@ -1,9 +1,9 @@
 {.passC: "-I" & currentSourcePath().substr(0, high(currentSourcePath()) - 4) .}
 
 when defined(macosx):
-    {.compile("webview.cc","-DWEBVIEW_COCOA=1 -DOBJC_OLD_DISPATCH_PROTOTYPES=1 -std=c++11 -framework WebKit").}
+    #{.compile("webview.cc","-DWEBVIEW_COCOA=1 -DOBJC_OLD_DISPATCH_PROTOTYPES=1 -std=c++11 -framework WebKit").}
 
-    {.passL: "-lstdc++ -framework WebKit".}
+    {.passL: "-L. -lwebview.o -lstdc++ -framework WebKit".}
     {.passC: "-DWEBVIEW_HEADER=1 -DWEBVIEW_COCOA=1".}
     
 
@@ -12,6 +12,8 @@ type
 
 proc createWebview*(debug: cint = 0, window: pointer = nil): WebviewT {.importc: "webview_create", header: "webview.h".}
 proc runWebview*(wv: WebviewT) {.importc: "webview_run", header: "webview.h".}
+proc setWebviewTitle*(wv: WebviewT, title: cstring) {.importc: "webview_set_title", header: "webview.h".}
+proc setWebviewSize*(wv: WebviewT, width: cint, height: cint, constraints: cint) {.importc: "webview_set_size", header: "webview.h".}
 proc navigateWebview*(wv: WebviewT, url: cstring) {.importc: "webview_navigate", header: "webview.h".}
 proc terminateWebview*(wv: WebviewT) {.importc: "webview_terminate", header: "webview.h".}
 proc destroyWebview*(wv: WebviewT) {.importc: "webview_destroy", header: "webview.h".}
