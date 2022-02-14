c769ebc5a1c062f5fdd4620027cc6713c018758d
drkameleon <yaniszaf@gmail.com>
2022-02-14 16:52:01 +0100

    added missing functions (almost) + code cleanup

--- webview.nim
+++ webview.nim
@@ -1,3 +1,18 @@
+######################################################
+# nim-webview
+# New-style Zaitsev's webview wrapper
+# for Nim
+#
+# (c) 2022 Yanis Zafirópulos
+# 
+# @license: see LICENSE file
+# @file: nim-webview.nim
+######################################################
+
+#=======================================
+# Compilation & Linking
+#=======================================
+
 {.compile("webview.cc","-std=c++11").}
 {.passC: "-I" & currentSourcePath().substr(0, high(currentSourcePath()) - 4) .}
 {.passL: "-lstdc++".}
@@ -16,6 +31,10 @@ elif defined(macosx):
 elif defined(windows):
     {.passC: "-DWEBVIEW_EDGE=1".}
     {.passL: "-mwindows -L./dll/x64 -lwebview -lWebView2Loader".}
+
+#=======================================
+# Types
+#=======================================
     
 type
     webview_t {.header: "webview.h", importc.} = pointer
@@ -29,29 +48,58 @@ type
         Maximum = 2
         Fixed = 3
 
-proc webview_create(debug: cint = 0, window: pointer = nil): webview_t {.header: "webview.h", importc.}
-proc webview_run(wv: webview_t) {.header: "webview.h", importc.}
-proc webview_set_title(wv: webview_t, title: cstring) {.header: "webview.h", importc.}
-proc webview_set_size(wv: webview_t, width: cint, height: cint, constraints: Constraints) {.header: "webview.h", importc.}
-proc webview_navigate(wv: webview_t, url: cstring) {.header: "webview.h", importc.}
-proc webview_terminate(wv: webview_t) {.header: "webview.h", importc.}
-proc webview_destroy(wv: webview_t) {.header: "webview.h", importc.}
+#=======================================
+# Function prototypes
+#=======================================
 
-proc newWebview*(
-    title: string, 
-    width: int, 
-    height: int, 
-    constraints = Default, 
-    debug=false): Webview =
-    
-    var wv = webview_create(debug.cint)
-    webview_set_title(wv, title=title.cstring)
-    webview_set_size(wv, width.cint, height.cint, constraints)
-    Webview(view: wv)
+proc webview_create(debug: cint = 0, window: pointer = nil): webview_t {.header: "webview.h", importc.}
+proc webview_destroy(w: webview_t) {.header: "webview.h", importc.}
+proc webview_run(w: webview_t) {.header: "webview.h", importc.}
+proc webview_terminate(w: webview_t) {.header: "webview.h", importc.}
+# webview_dispatch not implemented yet
+proc webview_get_window(w: webview_t): pointer {.header:"webview.h", importc.}
+proc webview_set_title(w: webview_t, title: cstring) {.header: "webview.h", importc.}
+proc webview_set_size(w: webview_t, width: cint, height: cint, constraints: Constraints) {.header: "webview.h", importc.}
+proc webview_navigate(w: webview_t, url: cstring) {.header: "webview.h", importc.}
+proc webview_init(w: webview_t, js: cstring) {.header: "webview.h", importc.}
+proc webview_eval(w: webview_t, js: cstring) {.header: "webview.h", importc.}
+# webview_bind not implemented yet
+# webview_return not implemented yet
+
+#=======================================
+# Main methods
+#=======================================
+
+proc newWebview*(title: string, width: int, height: int, constraints = Default, debug=false): Webview =
+    var w = webview_create(debug.cint)
+    webview_set_title(w, title=title.cstring)
+    webview_set_size(w, width.cint, height.cint, constraints)
+    Webview(view: w)
+
+method setTitle*(this: Webview, title: string) {.base.} =
+    webview_set_title(this.view, title=title.cstring)
+
+method setSize*(this: Webview, width: int, height: int, constraints: Constraints) =
+    webview_set_size(this.view, width=width.cint, height=height.cint, constraints)
 
 method navigateTo*(this: Webview, url: string) {.base.} =
     webview_navigate(this.view, url.cstring)
 
-method show*(this: Webview) {.base.} =
+method run*(this: Webview) {.base.} =
     webview_run(this.view)
+
+method inject*(this: Webview, js: string) {.base} =
+    webview_init(this.view, js.cstring)
+
+method eval*(this: Webview, js: string) {.base} =
+    webview_eval(this.view, js.cstring)
+
+method terminate*(this: Webview) {.base.} =
+    webview_terminate(this.view)
+
+method destroy*(this: Webview) {.base.} =
     webview_destroy(this.view)
+
+method show*(this: Webview) {.base.} =
+    this.run()
+    this.destroy()
