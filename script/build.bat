23e277bbb37162f536c18ba735286ca8ffea60de
drkameleon <yaniszaf@gmail.com>
2022-02-14 17:52:14 +0100

    experimenting with the Windows CI workflow

---
+++ script/build.bat
@@ -0,0 +1,20 @@
+echo Looking for vswhere.exe...
+set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%vswhere%" set "vswhere=%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%vswhere%" (
+	echo ERROR: Failed to find vswhere.exe
+	exit 1
+)
+echo Found %vswhere%
+
+echo Looking for VC...
+for /f "usebackq tokens=*" %%i in (`"%vswhere%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set vc_dir=%%i
+)
+if not exist "%vc_dir%\Common7\Tools\vsdevcmd.bat" (
+	echo ERROR: Failed to find VC tools x86/x64
+	exit 1
+)
+echo Found %vc_dir%
+
+call "%vc_dir%\Common7\Tools\vsdevcmd.bat" -arch=x86 -host_arch=x64
