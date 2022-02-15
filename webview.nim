c65cf4ed1e5d37142dda2426ebb919da5c422d02
drkameleon <yaniszaf@gmail.com>
2022-02-15 14:00:55 +0100

    fixed path

--- webview.nim
+++ webview.nim
@@ -33,7 +33,7 @@ elif defined(macosx):
 elif defined(windows):
     {.compile("webview.cc","/std:c++17 /EHsc").}
     {.passC: "-DWEBVIEW_EDGE=1".}
-    {.passL: """/EHsc /std:c++17 "libs\x64\WebView2Loader.dll.lib"""".}
+    {.passL: """/EHsc /std:c++17 "deps\libs\x64\WebView2Loader.dll.lib"""".}
 
 #=======================================
 # Types
