1ddd0ca67727dc98bee88cfef96fc5fc7f1521cd
drkameleon <yaniszaf@gmail.com>
2022-02-15 17:32:48 +0100

    fixed syntax

--- test.nim
+++ test.nim
@@ -1,5 +1,4 @@
 import webview
 
-var wt = newWebview("TEST", 400, 400)
-wt.navigateTo("data:text/html, <h1>It worked! Yay!</h1>")
+var wt = newWebview("TEST", "data:text/html, <h1>It worked! Yay!</h1>")
 wt.show()
