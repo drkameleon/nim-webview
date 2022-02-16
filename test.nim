d1708a3f5266c1eb6962ec6c0c380b45a00089fe
drkameleon <yaniszaf@gmail.com>
2022-02-16 12:08:15 +0100

    minor tweaks

--- test.nim
+++ test.nim
@@ -1,4 +1,13 @@
 import webview
 
-var wt = newWebview("TEST", "data:text/html, <h1>It worked! Yay!</h1>")
+var wt = newWebview("TEST", "data:text/html, <h1>It worked! Yay!</h1>", debug=true)
+
+# proc receiver(seq: cstring, req: cstring, arg: pointer) {.cdecl.} =
+#     echo "SEQ: " & $(seq)
+#     echo "GOT: " & $(req)
+#     echo "arg: " & $(cast[int](arg))
+
+#     wt.webview_return(seq, 0.cint, ($ %*("this is a message")).cstring)
+
+# wt.webview_bind("something", receiver, cast[pointer](666))
 wt.show()
